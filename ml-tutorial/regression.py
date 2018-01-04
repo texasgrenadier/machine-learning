6e8adf6d7e41dbdea2a8eca275592a1203af9054
Chuck Lemons <chuck@southerncolumbine.com>
2018-01-03 22:32:45 -0600

    Updates based on tutorial 4 - split data into train/test and run linear regression classifier

--- ml-tutorial/regression.py
+++ ml-tutorial/regression.py
@@ -1,9 +1,13 @@
 import math
+import numpy as np
 import pandas as pd
 import quandl
+from sklearn import model_selection
+from sklearn import preprocessing, svm
+
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
-print(df.head())
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
@@ -14,8 +18,20 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-print(df.head())
+x = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+x = preprocessing.scale(x)
+y = np.array(df['label'])
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+
+classifier = LinearRegression()
+classifier.fit(x_train, y_train)
+accuracy = classifier.score(x_test, y_test)
+
+print(accuracy)
